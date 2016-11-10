7519c9673238bd48006edc23b4308e504df9175c
Salvatore Polito <thedarkhacker94@gmail.com>
2016-11-10 22:31:15 +0100

    Initial Commit

---
+++ OCRtest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OCRtest
+//
+//  Created by Salvatore  Polito on 10/11/16.
+//  Copyright © 2016 Salvatore  Polito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
